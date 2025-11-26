3b41afcd1f3b34aa2d7c89fb2c979d0d960c25ed
Sri Nivas <raopsrinivasa43@gmail.com>
2025-11-26 20:30:14 +0530

    task-1 complete: implemented CRUD endpoints and added reflection answers

--- CodeChallenge.Api/Controllers/MessagesController.cs
+++ CodeChallenge.Api/Controllers/MessagesController.cs
@@ -21,34 +21,65 @@ public class MessagesController : ControllerBase
     public async Task<ActionResult<IEnumerable<Message>>> GetAll(Guid organizationId)
     {
         // TODO: Implement
-        throw new NotImplementedException();
+        var messages = await _repository.GetAllByOrganizationAsync(organizationId);
+        return Ok(messages);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Message>> GetById(Guid organizationId, Guid id)
     {
         // TODO: Implement
-        throw new NotImplementedException();
+        var message = await _repository.GetByIdAsync(organizationId, id);
+        if (message == null)
+        {
+            return NotFound();
+        }
+        return Ok(message);
     }
 
     [HttpPost]
     public async Task<ActionResult<Message>> Create(Guid organizationId, [FromBody] CreateMessageRequest request)
     {
         // TODO: Implement
-        throw new NotImplementedException();
+        var newMessage = new Message
+        {
+            OrganizationId = organizationId,
+            Title = request.Title,
+            Content = request.Content,
+            CreatedAt = DateTime.UtcNow,
+            IsActive = true,
+        };
+
+        var created = await _repository.CreateAsync(newMessage);
+        return CreatedAtAction(nameof(GetById), new { organizationId, id = created.Id }, created);
     }
 
     [HttpPut("{id}")]
     public async Task<ActionResult> Update(Guid organizationId, Guid id, [FromBody] UpdateMessageRequest request)
     {
         // TODO: Implement
-        throw new NotImplementedException();
+        var existing = await _repository.GetByIdAsync(organizationId, id);
+        if(existing == null) 
+            return NotFound();
+
+        existing.Title = request.Title;
+        existing.Content = request.Content;
+
+        var updated = await _repository.UpdateAsync(existing);
+        if(updated == null)
+            return NotFound();
+
+        return NoContent();
     }
 
     [HttpDelete("{id}")]
     public async Task<ActionResult> Delete(Guid organizationId, Guid id)
     {
         // TODO: Implement
-        throw new NotImplementedException();
+        var deleted = await _repository.DeleteAsync(organizationId, id);
+        if(!deleted)
+            return NotFound();
+
+        return NoContent();
     }
 }
